1e90aa68cc2e0db9d259d0c0e0bb6968b82b41bb
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-11 21:37:40 +0100

    exclude SmokeTests from docfx

--- build-docs.bat
+++ build-docs.bat
@@ -1,4 +1,5 @@
 xcopy "..\..\..\Library\ScriptAssemblies\Luny*" "..\..\..\Library\XmlDocs\" /Y /I
+del "..\..\..\Library\XmlDocs\*SmokeTests*"
 xcopy api-index.md api\index.md /Y /-I
 docfx metadata docfx.json
 docfx build docfx.json
